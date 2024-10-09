7c183e5d4216e48139e11c58460d1048ab6ff72e
syedazw <syedazw@gmail.com>
2024-10-09 22:11:40 +0500

    all file push

---
+++ pages/assets/js/carousel.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+    var owl = $('.choose_us_section .owl-carousel');
+    owl.owlCarousel({
+        margin: 30,
+        nav: true,
+        loop: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            }
+        }
+    })
+})
+
+$(document).ready(function() {
+    var owl = $('.our_news_section .owl-carousel');
+    owl.owlCarousel({
+        margin: 30,
+        nav: true,
+        loop: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    })
+})
+
+$(document).ready(function() {
+    var owl = $('.testimonials_section .owl-carousel');
+    owl.owlCarousel({
+        margin: 30,
+        nav: true,
+        loop: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+})
+
+$(document).ready(function() {
+    var owl = $('.provide_section .owl-carousel');
+    owl.owlCarousel({
+        margin: 30,
+        nav: true,
+        loop: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            }
+        }
+    })
+})
